45f1a8b984db5768d345ac9f5b4f9ff6df754e87
Raja Moreno <rajamoreno@Rajas-MBP.stdusr.yale.internal>
2024-04-21 15:47:13 -0400

    Updates to oyez_scraper.py

--- oyez_scraper.py
+++ oyez_scraper.py
@@ -1,44 +1,51 @@
+# scotus-predictor/oyez_scraper.py
+# author: rajamoreno
+
 import oyez_api_wrapper
 import json
 
-YEAR_RANGE = [2000, 2001]
+# This script relies on first downloading a file called cases.json, accessible via Oyez's unlisted API at:
+#   http://api.oyez.org/cases?per_page=0
+# A current version
 
-assert YEAR_RANGE[0] <= YEAR_RANGE[1]
-assert 1776 <= YEAR_RANGE[0]
-assert YEAR_RANGE[1] <= 2024
+# With gratitude to:
+# tylzars, the author of oyez_api_wrapper, which made processing Oyez's data much easier
+#   https://github.com/tylzars/oyez-api-wrapper
+# walkerdb, for describing his experimentation with the Oyez API here:
+#   https://github.com/walkerdb/supreme_court_transcripts/issues/1#issuecomment-377104119
+# Real Python, for their tutorial on using json data in Python:
+#   https://realpython.com/python-json/
 
-years = []
-curr_year = YEAR_RANGE[0]
-while curr_year <= YEAR_RANGE[1]:
-    years.append(curr_year)
-    curr_year += 1
-print("YEARS TO BE SCRAPED: ", years)
+with open("cases.json", "r") as read_file:
+    data = json.load(read_file)
+    print("NUMBER OF ENTRIES: ", len(data))
+    for i in range(3):
+        href = data[i]['href']
+        # print(href)
+        href_split = href.split("/")
+        # print(href_split)
+        year = href_split[-2]
+        case_number = href_split[-1]
+        print(year, case_number)
 
-# case_obj = oyez_api_wrapper.court_case("2015", "15-278")
-# print(case_obj.get_case_judges())    
-
-# now pull all the data
-
-for year in years:
-    case_numbers = ["00-24", "01-46"]
-    # case_numbers = generate_supreme_court_case_numbers(year, MAX_CASE_NUMBER)
-    for case_number in case_numbers:
-        try:
-            print("YEAR: {} CASE NUMBER: {}".format(year, case_number))
-            case_obj = oyez_api_wrapper.court_case(year, case_number)
-            print(case_obj.get_ruling())
-        except:
-            print("FAILURE")
-        else:
-            print("SUCCESS")
-            print(case_obj.get_case_judges())
-            print(case_obj.get_case_facts())
-            print(case_obj.get_judge_decisions())
-            print(case_obj.get_ruling())
-            print(case_obj.get_basic_info())
-            print(case_obj.get_legal_question())
-            print(case_obj.get_conclusion())
-            print(case_obj.get_lower_court())
+        # This data collection uses oyez_api_wrapper, but handling of NoneType is inconsistent,
+        # so instead I'm rewriting this for myself.
+        case_obj = oyez_api_wrapper.court_case(year, case_number)
+        case_basic_info = case_obj.get_basic_info()
+        print(case_basic_info)
+        case_judges = case_obj.get_case_judges()
+        print(case_judges)
+        case_facts = case_obj.get_case_facts()
+        print(case_facts)
+        case_legal_question = case_obj.get_legal_question()
+        print(case_legal_question)
+        case_ruling = case_obj.get_ruling()
+        print(case_ruling)
+        case_conclusion = case_obj.get_conclusion()
+        print(case_conclusion)
+        print(case_obj.get_judge_decisions())
+        case_lower_court = case_obj.get_lower_court()
+        print(case_lower_court)
 
 
 
